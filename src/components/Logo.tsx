e0879c9805291c9e8e224ccad7ae1d83c9caeb05
[dyad] <toyesterdayagencia@gmail.com>
2025-09-17 21:36:47 -0300

    [dyad] Adicionando a logo e ajustando a paleta de cores - wrote 2 file(s)

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo: React.FC = () => {
+  return (
+    <img
+      src="/logo.svg" // Certifique-se de que o arquivo logo.svg esteja na pasta 'public'
+      alt="Ana Pais Arquitetura Logo"
+      className="h-10 w-auto" // Ajuste o tamanho conforme necessário
+    />
+  );
+};
+
+export default Logo;
