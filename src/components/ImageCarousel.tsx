811f1cdfc092abb0ff9c9efdc5bcbfc8ae5802ad
[dyad] <toyesterdayagencia@gmail.com>
2025-10-10 22:38:16 -0300

    [dyad] Corrigindo o layout do carrossel e a responsividade geral em dispositivos móveis - wrote 3 file(s)

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -21,14 +21,14 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ images, altText }) => {
   return (
     <Carousel
       plugins={[plugin.current]} // Aplica o plugin de autoplay
-      className="w-full h-full"
+      className="w-full h-full relative" // Adicionado 'relative' para garantir o posicionamento correto das setas
       opts={{
         loop: true, // Garante que o carrossel irá repetir as imagens
       }}
     >
-      <CarouselContent>
+      <CarouselContent className="h-full"> {/* Adicionado h-full aqui */}
         {images.map((imageSrc, index) => (
-          <CarouselItem key={index}>
+          <CarouselItem key={index} className="h-full"> {/* Adicionado h-full aqui */}
             <img
               src={imageSrc}
               alt={`${altText} ${index + 1}`}
