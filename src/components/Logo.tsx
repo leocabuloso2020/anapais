617a2fc907f492bd1cabcf7cfde06ec1ae41af92
[dyad] <toyesterdayagencia@gmail.com>
2025-10-10 21:12:09 -0300

    [dyad] Atualizando o logo SVG e removendo o filtro de cor - wrote 2 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
     <img
       src="/logo.svg" // Certifique-se de que o arquivo logo.svg esteja na pasta 'public'
       alt="Ana Pais Arquitetura Logo"
-      className="h-[22.4rem] w-auto filter brightness(0) invert(1)" // Adicionado filtro para forçar a cor branca
+      className="h-[22.4rem] w-auto" // Removido o filtro de cor, o SVG deve ser branco por padrão agora
     />
   );
 };
