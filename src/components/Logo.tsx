2439f7f6647cc53799e55a0037214a61b9493c29
[dyad] <toyesterdayagencia@gmail.com>
2025-10-10 21:13:34 -0300

    [dyad] wrote 2 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Logo: React.FC = () => {
   return (
     <img
-      src="/logo.svg" // Certifique-se de que o arquivo logo.svg esteja na pasta 'public'
+      src="/logo.svg" // O arquivo logo.svg na pasta 'public' foi atualizado
       alt="Ana Pais Arquitetura Logo"
-      className="h-[22.4rem] w-auto" // Removido o filtro de cor, o SVG deve ser branco por padrão agora
+      className="h-[22.4rem] w-auto" // Nenhuma classe de filtro de cor é necessária, pois o SVG já é branco
     />
   );
 };
