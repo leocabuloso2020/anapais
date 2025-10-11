aaef7892a9c3b351d5c999285637c6c9e7023950
[dyad] <toyesterdayagencia@gmail.com>
2025-10-10 21:09:14 -0300

    [dyad] Garantindo que o logo seja branco - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
     <img
       src="/logo.svg" // Certifique-se de que o arquivo logo.svg esteja na pasta 'public'
       alt="Ana Pais Arquitetura Logo"
-      className="h-[22.4rem] w-auto filter invert" // Aumentado em 40% (de h-64 para h-[22.4rem])
+      className="h-[22.4rem] w-auto" // Removida a classe 'filter invert'
     />
   );
 };
