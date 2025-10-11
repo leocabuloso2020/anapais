f99f3024cfd6e56b844ee369563b3ccfeec8b396
[dyad] <toyesterdayagencia@gmail.com>
2025-10-10 22:11:42 -0300

    [dyad] Ajustando o tamanho do ícone do WhatsApp para 72px - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -17,7 +17,7 @@ const WhatsAppButton: React.FC = () => {
         className="rounded-full w-20 h-20 flex items-center justify-center shadow-lg bg-brand-color hover:bg-brand-color-600 transition-colors duration-300"
         aria-label="Fale conosco pelo WhatsApp"
       >
-        <MessageCircle size={64} className="text-white" /> {/* Usando a prop 'size' para controlar o tamanho */}
+        <MessageCircle size={72} className="text-white" /> {/* Aumentado para size={72} */}
       </Button>
     </a>
   );
